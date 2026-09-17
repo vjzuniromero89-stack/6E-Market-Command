8e5efe3ab88abcab7bff685247873eb9ca1f3afc
vjzuniromero89-stack <vjzuniromero89@gmail.com>
2026-09-17 09:14:08 -0400

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'6E Market Command',description:'EUR/USD intermarket trading dashboard'};
+export default function Layout({children}){return <html lang="en"><body>{children}</body></html>}
